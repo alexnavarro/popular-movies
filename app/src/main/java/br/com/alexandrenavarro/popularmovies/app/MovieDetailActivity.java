a6fecceb7f6a36849568bffcaaad8912fb642494
alexandrenavarro <alexandrenavarro@gmail.com>
2016-12-27 12:18:52 -0200

    fixing bug detail image

--- app/src/main/java/br/com/alexandrenavarro/popularmovies/app/MovieDetailActivity.java
+++ app/src/main/java/br/com/alexandrenavarro/popularmovies/app/MovieDetailActivity.java
@@ -8,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -29,12 +30,16 @@ import static br.com.alexandrenavarro.popularmovies.app.MainActivity.REQUEST_COD
 
 public class MovieDetailActivity extends AppCompatActivity {
 
+    private static final String TAG_LOG = MovieDetailActivity.class.getSimpleName();
+
+
     private TextView mTxtTitle;
     private TextView mTxtSynopsis;
     private TextView mTxtReleaseYear;
     private TextView mTxtRate;
-        private ImageView mImvMovie;
+    private ImageView mImvMovie;
     private Movie mMovie;
+    private Target target = new CustomTarget();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -74,42 +79,14 @@ public class MovieDetailActivity extends AppCompatActivity {
     }
 
     public void bind(){
+        String path = MovieDBImageURLBuilder.buildURL(mMovie.getPosterPath());
+        Log.d(TAG_LOG, path);
         Picasso.with(getApplicationContext()).
-                load(MovieDBImageURLBuilder.buildURL(mMovie.getPosterPath()))
+                load(path)
                 .centerCrop().resize((int)PxConverter.convertDpToPixel(120f, getApplicationContext()),
                 (int)PxConverter.convertDpToPixel(170f, getApplicationContext()))
-                .into(new Target() {
-                    @Override
-                    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                        assert mImvMovie != null;
-                        mImvMovie.setImageBitmap(bitmap);
-                        Palette.from(bitmap)
-                                .generate(new Palette.PaletteAsyncListener() {
-                                    @Override
-                                    public void onGenerated(Palette palette) {
-                                        Palette.Swatch textSwatch = palette.getDarkMutedSwatch();
-
-                                        if(textSwatch != null) {
-                                            mTxtTitle.setBackgroundColor(textSwatch.getRgb());
-                                            mTxtTitle.setTextColor(textSwatch.getBodyTextColor());
-                                        }else {
-                                            mTxtTitle.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), android.R.color.holo_orange_light));
-                                        }
-
-                                    }
-                                });
-                    }
-
-                    @Override
-                    public void onBitmapFailed(Drawable errorDrawable) {
-
-                    }
-
-                    @Override
-                    public void onPrepareLoad(Drawable placeHolderDrawable) {
-
-                    }
-                });
+                .into(target);
+        
         mTxtTitle.setText(mMovie.getTitle());
         mTxtRate.setText(Double.toString(mMovie.getRating()));
         mTxtSynopsis.setText(mMovie.getSynopsis());
@@ -124,5 +101,39 @@ public class MovieDetailActivity extends AppCompatActivity {
         }
 
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private class CustomTarget implements Target{
+
+        @Override
+        public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+            assert mImvMovie != null;
+            mImvMovie.setImageBitmap(bitmap);
+            Palette.from(bitmap)
+                    .generate(new Palette.PaletteAsyncListener() {
+                        @Override
+                        public void onGenerated(Palette palette) {
+                            Palette.Swatch textSwatch = palette.getDarkMutedSwatch();
+
+                            if(textSwatch != null) {
+                                mTxtTitle.setBackgroundColor(textSwatch.getRgb());
+                                mTxtTitle.setTextColor(textSwatch.getBodyTextColor());
+                            }else {
+                                mTxtTitle.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), android.R.color.holo_orange_light));
+                            }
+
+                        }
+                    });
+        }
+
+        @Override
+        public void onBitmapFailed(Drawable errorDrawable) {
+            Log.d(TAG_LOG, errorDrawable.toString());
+        }
+
+        @Override
+        public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+        }
     }
 }
